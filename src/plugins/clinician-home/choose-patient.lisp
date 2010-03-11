8e7d5079fe258c0fa2ed3b4c3bacd61f2f4847b8
Keith Corbett <none>
2010-03-11 11:27:19 -0800

    CHOOSE-PATIENT widget now can mark siblings dirty if changed patient
    Ignore-this: 6695ac3494f2c6976b998fc20a7af616
    value. Slot accessor MARK-SIBLINGS-DIRTY-P and init arg :MARK-SIBLINGS-DIRTY-P.
    Useful in parent widgets where changing patient needs to propagate
    changes to sibling widgets. (see survey-ctrl.lisp)
    
    darcs-hash:20100311192719-f85d5-b2598d2b6b9d14e4ae48c1eab2e4157d82fce867.gz

--- src/plugins/clinician-home/choose-patient.lisp
+++ src/plugins/clinician-home/choose-patient.lisp
@@ -5,45 +5,54 @@
 (registry-proclamations)
 
 (defwidget choose-patient ()
-  ((hr-p :accessor choose-patient-hr-p :initarg :hr-p :initform t)))
+  ((hr-p :accessor choose-patient-hr-p :initarg :hr-p :initform t)
+   (mark-siblings-dirty-p :accessor mark-siblings-dirty-p :initarg :mark-siblings-dirty-p :initform nil)))
 
 (defmethod render-widget-body ((widget choose-patient) &rest args)
   (declare (ignore args))
   (let* ((user (current-user))
          (center (current-center))
          (patients (and center (get-patients-for-center center)))
-         (user-patient (and user (get-patient-for-user user))))
+         (user-patient (and user (get-patient-for-user user)))
+         invalidate)
     (when user-patient
       (pushnew user-patient patients :test #'eq))
     (with-html
       (cond ((null patients)
-             (setf (current-patient) nil)
+             (setf (current-patient) nil
+                   invalidate t)
              (htm (str #!"You do not have access to any patients")))
             ((null (cdr patients))
              (let ((patient (car patients)))
                (unless (eq patient (current-patient))
-                 (setf (current-patient) patient))
+                 (setf (current-patient) patient
+                       invalidate t))
                (htm
                 "<b>Patient:</b> "
                 (str (id patient)))))
             (t (let ((patient (current-patient))
                      (action (lambda (&key patient &allow-other-keys)
                                (cond ((get-patient patient center t)
-                                      (setf (current-patient) patient))
+                                      (setf (current-patient) patient)
+                                      (if (mark-siblings-dirty-p widget)
+                                          (mark-dirty (car (last (widget-parents widget))))))
                                      (t
                                       ;; Somebody deleted or renamed the patient
                                       (mark-dirty widget))))))
                  (setf patients (sort patients #'string-lessp :key #'id))
                  (unless (member patient patients :test #'eq)
                    (setf patient (car patients)
-                         (current-patient) patient))
+                         (current-patient) patient
+                         invalidate t))
                  (with-html-form (:get action :use-ajax-p t :class "autodropdown")
                    "<b>Patient:</b> "
                    (render-dropdown "patient" (mapcar 'id patients)
                                     :selected-value (id patient)
                                     :autosubmitp t)))))
       (when (choose-patient-hr-p widget)
-        (htm (:hr))))))
+        (htm (:hr))))
+    (when (and invalidate (mark-siblings-dirty-p widget))
+      (mark-dirty (car (last (widget-parents widget)))))))
 
-(defun make-choose-patient-widget (&key (hr-p t))
-  (make-instance 'choose-patient :hr-p hr-p))
+(defun make-choose-patient-widget (&key (hr-p t) (mark-siblings-dirty-p nil))
+  (make-instance 'choose-patient :hr-p hr-p :mark-siblings-dirty-p mark-siblings-dirty-p))
