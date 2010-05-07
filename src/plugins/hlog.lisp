9d11c716ed82705691e3d6e71baa1358028ea028
rme <rme@clozure.com>
2010-05-07 10:16:47 -0700

    Update hlog to support json aprotocol
    darcs-hash:20100507171647-a1430-88e40c73bbf9a878feb561ce9a63a162161100d1.gz

--- src/plugins/hlog.lisp
+++ src/plugins/hlog.lisp
@@ -1,15 +1,18 @@
 (in-package :registry)
 
 (define-api-handler (hlog :mime "text/json") (tokens params auth-p)
-  (let ((command (first tokens)))
+  (let ((command (first tokens))
+	(body (awhen (hunchentoot::raw-post-data :force-text t)
+		(json:decode-json-from-string it))))
     (cond ((equalp command "Authenticate")
-	   (hlog-authenticate-command params auth-p))
+	   (prog1 (hlog-authenticate-command body auth-p)
+	     (format t "~A~%" (webapp-session-value weblocks::*authentication-key*))))
 	  ((not auth-p)
 	   (hlog-error :authentication (first tokens)))
 	  ((equalp command "GetSurveyUpdates")
-	   (hlog-update-surveys-command params))
+	   (hlog-update-surveys-command body params))
 	  ((equalp command "UpdateAnswers")
-	   (hlog-update-answers-command params))
+	   (hlog-update-answers-command body))
 	  (t (hlog-error :unknown-command (first tokens))))))
 
 (defun hlog-error (type command &optional params)
@@ -22,26 +25,28 @@
       (with-html
 	(json:encode-json-to-string
 	 `(("authenticated" . "true")
-	   ("token" . auth-p))))
+	   ("token" . ,auth-p))))
       (with-html
 	(json:encode-json-to-string
 	 `(("authenticated" . "false"))))))
 	   
 (defun hlog-authenticate-p (params)
-  (authenticate-user (assoc-get :username params)
-		     (assoc-get :password params)))
+  (awhen (authenticate-user (assoc-get :username params)
+			    (assoc-get :password params))
+    (set-session-user it)))
 
 
 ;;
 ;; Sync with server
 ;;
 
-(defun hlog-update-surveys-command (params)
-;;  (hunchentoot:raw-post-data hunchentoot::*request*)
-  (with-html
-    (json:encode-json-to-string
-     `(("Survey List" . ,(get-survey-list params))))))
-
+(defun hlog-update-surveys-command (message params)
+  (declare (ignore message))
+  (format t "~A~%" params)
+  (if (assoc-get :test params)
+      (json:encode-json-to-string *update-response*)
+      (json:encode-json-to-string
+       `(("Survey List" . ,(get-survey-list params))))))
 
 (defun get-survey-list (params)
   "Given params, return survey for authenticated user"
@@ -66,3 +71,32 @@
     (if (assoc-get :surveyanswer params)
 	"YES"
 	"NO")))
+
+;;
+;; Testing from client
+;;
+
+(defparameter *cookie-jar*
+  (make-instance 'drakma:cookie-jar))
+
+(defparameter *update-request*
+  '((:*SURVEY\ *LIST (:*SURVEYS ((:+ID+ . "SID1") (:*TYPE . "Dairy") (:*LAST-UPDATED-TIME-STAMP . "12000930")) ((:+ID+ . "SID1") (:*LAST-UPDATED-TIME-STAMP . "12000930")) ((:+ID+ . "SID2") (:*LAST-UPDATED-TIME-STAMP . "12000930"))))))
+
+(defparameter *update-response*
+  `((:*SURVEY\ *LIST (:*SURVEYS ((:+ID+ . "SID1") (:*NAME . "Diabetes") (:*TYPE . "Dairy") (:*FETCHED-TIME-STAMP . "12000931") (:*SURVEY-+XML+ . "<SURVEYXML_AS_IN_XML_SCHEMA>")) ((:+ID+ . "SID2") (:*NAME . "Cancer") (:*TYPE . "survey") (:*FETCHED-TIME-STAMP . "12000931") (:*SURVEY-+XML+ . "<SURVEYXML_AS_IN_XML_SCHEMA>"))) (:*OBSOLETE ((:+ID+ . "SID23")) ((:+ID+ . "SID45"))))))
+
+(defun test-hlog-authenticate (user pw)
+  (drakma:http-request "http://www.lamsight.org:8081/data/hlog/Authenticate"
+		       :method :post
+		       :cookie-jar *cookie-jar*
+		       :content (json:encode-json-alist-to-string 
+				 `((:username . ,user) (:password . ,pw)))
+		       :content-type "text/json"))
+
+(defun test-hlog-update (&key (request *update-request*) test-p)
+  (drakma:http-request "http://www.lamsight.org:8081/data/hlog/GetSurveyUpdates"
+		       :method :post
+		       :parameters (when test-p '(("test" . "t")))
+		       :cookie-jar *cookie-jar*
+		       :content (json:encode-json-alist-to-string request)
+		       :content-type "text/json"))
